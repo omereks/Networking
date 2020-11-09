edd8764fe68f0b8873f40fb623c986c8c13c6f4b
AvrahamBi <avrahambarilan@gmail.com>
2020-11-09 14:43:29 +0200

    hello omer

--- client.py
+++ client.py
@@ -1,8 +1,13 @@
 import socket
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', 60237))
-s.sendto( b"avraham and omer , 205937949", ('127.0.0.1', 12345))
-data, addr = s.recvfrom(1024)
-print(str(data), addr)
-s.close()
+serverIP = sys.argv[1]
+serverPort = sys.argv[2]
+while True:
+    siteAddress = input('Enter site address')
+    b = bytes(siteAddress, 'utf-8')
+    s.sendto(b, (serverIP, serverPort))
+    s.bind(('', 60237))                    # TODO maybe should remove it
+    data, addr = s.recvfrom(1024)
+    print(str(data), addr)
